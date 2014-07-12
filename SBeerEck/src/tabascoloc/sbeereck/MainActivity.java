778d03132e5d4846d69438a3fd7e0c2fc98a83c7
SyefTriple <syef_the_best@hotmail.fr>
2014-07-12 18:21:51 +0200

    Ajout d'un bouton quitter sur l'écran principal

--- SBeerEck/src/tabascoloc/sbeereck/MainActivity.java
+++ SBeerEck/src/tabascoloc/sbeereck/MainActivity.java
@@ -4,14 +4,23 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnTouchListener, View.OnClickListener {
+	private Button b = null;
+	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        b = (Button) findViewById(R.id.button1);
+        b.setOnTouchListener(this);
+        b.setOnClickListener(this);
     }
 
 
@@ -33,4 +42,17 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+	@Override
+	public void onClick(View v) {
+		this.finish();
+	}
+
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
