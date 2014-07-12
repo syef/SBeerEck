16a107355e6b00acf59c9a50d5dca7c903bde8a5
SyefTriple <syef_the_best@hotmail.fr>
2014-07-12 15:32:30 +0200

    test

--- SBeerEck/src/tabascoloc/sbeereck/MainActivity.java
+++ SBeerEck/src/tabascoloc/sbeereck/MainActivity.java
@@ -1,17 +1,22 @@
 package tabascoloc.sbeereck;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
-
+	private TextView texte = null;
+		
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        
+        texte = new TextView(this);
+        texte.setText("Bonjour, vous me devez 1 000 000€.");
+        setContentView(texte);
     }
 
 
